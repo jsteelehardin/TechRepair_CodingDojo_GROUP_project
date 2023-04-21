24587a38a2e944f9cd955b53e97124833fbc9c9e
Jessica Steele-Hardin <115476169+jsteelehardin@users.noreply.github.com>
2023-04-21 14:21:02 -0400

    Update repairs.py

---
+++ flask_app/controllers/repairs.py
@@ -0,0 +1,70 @@
+from flask import render_template,redirect,session,request, flash
+from flask_app import app
+from flask_app.models import repair
+
+@app.route('/new_repair')
+def request_repair():
+    return render_template('repair.html')
+
+@app.route('/repair_submission', methods=['POST'])
+def request_submit(): 
+    if not repair.Repair_request.validate(request.form):
+        return redirect('/new_repair')
+    data = {
+        'name' : request.form['name'],
+        'location' : request.form['location'],
+        'destination' : request.form['destination'],
+        'user_id_posted' : request.form['user_id_posted'],
+        'user_id_worker' : request.form['user_id_worker']
+    }
+    repair.Repair.save(data)
+    return redirect('/repair_dashboard')
+
+@app.route('/delete_repair/<int:id>')
+def destroy(id):
+    data = {
+        'id' : id
+    }
+    repair.Repair.delete(data)
+    return redirect('/repair_dashboard')
+
+@app.route('/become_worker', methods =['POST'])
+def update_driver():
+    data = {
+        'repair_id' : request.form['repair_id'],
+        'user_id_worker' : session['user_id']
+    }
+    repair.Repair.become_worker(data)
+    return redirect('/repair_dashboard')
+
+@app.route('/detail_page/<int:id>')
+def show_details(id):
+    data = {
+        'id' : id
+    }
+    return render_template('details.html',repair=repair.Repair.get_one_wit_users(data))
+
+@app.route('/edit_details/<int:id>')
+def display_edit_page(id):
+    data = {
+        'id' : id
+    }
+    return render_template('edit_details.html', repair = repair.Repair.get_one_by_id(data))
+
+@app.route('/submit_update', methods = ['POST'])
+def update_the_details():
+    data = {
+        'repair_id' : request.form['repair_id'],
+        'location' : request.form['location'],
+        'destination' : request.form['destination']
+    }
+    repair.Repair.update(data)
+    return redirect(f'/detail_page/{data["repair_id"]}')
+
+@app.route('/cancel_repair/<int:id>')
+def worker_canceled(id):
+    data = {
+        'repair_id' : id
+    }
+    repair.Repair.worker_cancel(data)
+    return redirect('/repair_dashboard')
